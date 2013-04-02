08ab47ecde7b31e28b910560daf2a3c6a1056499
dbetke <danielle.betke@gmail.com>
2013-04-02 13:29:19 -0400

    updates methods and tests for parsing monthly .dat file

--- spec/monthly.js
+++ spec/monthly.js
@@ -9,14 +9,14 @@ describe("monthly", function () {
 
     });
 
-    describe("parsing from test file data/USC00010008.normals.prcp.dat", function () {
+    describe("parsing from test file data/USC00010008.prcp.dat", function () {
 
         var loadingFinished = false,
             data,
             n;
 
         $.ajax({
-            url : '../data/USC00010008.normals.prcp.dat',
+            url : '../data/USC00010008.prcp.dat',
             dataType : 'text',
             success : function (response) {
                 loadingFinished = true;
@@ -35,8 +35,33 @@ describe("monthly", function () {
 
         describe("generate monthly", function () {
 
-            xit("should have a date", function () {
+            it("should accurately store a year", function () {
+                expect(n[1948]).toEqual(jasmine.any(Object));
+                expect(n[1948]).not.toEqual(jasmine.any(String));
+                expect(n[1947]).toBe(undefined);
+            });
 
+            it("should accurately store a month", function () {
+                expect(n[1948][7]).toEqual(jasmine.any(Array));
+                expect(n[1948][7]).not.toEqual(jasmine.any(String));
+                expect(n[1948][6]).toBe(undefined);
+            });
+
+            it('should store monthly values in an array', function () {
+                expect(n[1948][12][0]).toEqual(0);
+                expect(n[1949][11][12]).toEqual(173);
+                expect(n[1950][4][2]).toEqual(15);
+                expect(n[1950][12][15]).toEqual(0);
+                expect(n[2011][6][28]).toEqual(102);
+                expect(n[2013][1][1]).toEqual('M');
+            });
+
+            it('should store numbers as numbers and letters as strings', function () {
+                expect(n[2007][12][28]).toEqual(jasmine.any(String));
+                expect(n[2007][12][28]).not.toEqual(jasmine.any(Number));
+                expect(n[2006][10][3]).toEqual(jasmine.any(Number));
+                expect(n[2006][10][3]).not.toEqual(jasmine.any(String));
+                
             });
 
         });
--- src/monthly.js
+++ src/monthly.js
@@ -2,10 +2,35 @@ function generateMonthly(str) {
     'use strict';
 
     var obj = {},
-        fileArray = str.split('\n');
+        fileArray = str.split('\n'),
+        item,
+        lineItem,
+        date,
+        year,
+        month;
+    
+    for (item in fileArray) {
+        lineItem = fileArray[item].split(',');
+        date = lineItem[0];
+        year = Number(date.substr(0,4));
+        month = Number(date.substr(4));
+        
+        if (!obj[year]) {
+            obj[year] = {};
+        }
 
-//[ { year: … , month : … , values : [ … ]  } ]
+        obj[year][month] = [];
+
+        for (var i = 1; i < lineItem.length; i++) {
+            var value = lineItem[i];
+
+            if (!isNaN(Number(value))) {
+                value = Number(value);
+            } 
+
+            obj[year][month].push(value);
+        }
+    }
 
     return obj;
-
 }
